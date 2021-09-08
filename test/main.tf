8a6116af71263fa5358ed07983914e128a784316
thirumaldage <thirumaldage@gmail.com>
2021-09-08 07:38:46 +0530

    new file

---
+++ test/main.tf
@@ -0,0 +1,10 @@
+ #Please use terraform v12.29
+ 
+ provider "azurerm" {
+  version = "= 2.18"
+  features {}
+}
+resource "azurerm_resource_group" "myRG" {
+	name = "testRG"
+	location = "westeurope"
+}
