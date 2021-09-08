120d2620791f625c7348e34e581d16a57738a536
thirumal dage <thirumaldage@gmail.com>
2021-09-08 16:47:52 +0530

    removed other code

--- test/main.tf
+++ test/main.tf
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
  #Please use terraform v12.29
  
  provider "azurerm" {
@@ -8,15 +8,4 @@
 resource "azurerm_resource_group" "myRG" {
 	name = "testRG"
 	location = "westeurope"
-=======
- #Please use terraform v12.29
- 
- provider "azurerm" {
-  version = "= 2.18"
-  features {}
-}
-resource "azurerm_resource_group" "myRG" {
-	name = "testRG"
-	location = "westeurope"
->>>>>>> 1138ad00c9f136a88f788031c3aad5efe33962f4
 }
